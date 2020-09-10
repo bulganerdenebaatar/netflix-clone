3e5ec005cfaf5c4652231c4a9de1282a535ac8a1
Erdenebaatar <Erdenebaatar@ErdenebatarsAir.hsd1.wa.comcast.net>
2020-09-09 20:46:37 -0700

    created button primary

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import logo from '../svg/logo.svg';
 import { NavLink, Link } from 'react-router-dom';
+import { Button } from './Button';
 import styled from 'styled-components';
 import { Icon } from 'react-icons-kit';
 import { ic_keyboard_arrow_right } from 'react-icons-kit/md/ic_keyboard_arrow_right'
@@ -17,7 +18,7 @@ class Header extends Component {
                 <div className="header-content">
                     <Title> See What's Next</Title>
                     <SubTitle>WATCH ANYWHERE. CANCEL ANYTIME.</SubTitle>
-                    <Link className="main-offer-btn">try it now
+                    <Link className="main-offer-btn" primary>try it now
                     <Icon className="Icon" icon={ic_keyboard_arrow_right} ze={37} />
                     </Link>
                 </div>
@@ -76,26 +77,6 @@ const HeaderComponent = styled.div`
         text-align: center;
         flex-direction: column;
         z-index: 1;
-    }
-
-    // Main Offer Btn
-    .main-offer-btn {
-        display: inline-block;
-        background: var(--main-red);
-        text-transform: uppercase;
-        border: none;
-        outline: none;
-        margin: 0 33%;
-        padding: 1.5rem;
-        border-radius: 0.185rem;
-        font-size: 2rem;
-        text-align: center;
-        box-shadow: 0 1px 0 rgba(0, 0, 0, 0.45);
-        transition: background 0.2s ease-in;
-        cursor: pointer;
-        &:hover {
-            background: var(--main-red-hover);
-        }
     }
 
     .Icon svg {
---
+++ src/components/TabContentOne.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Img from '../images/tab-1-pic.png'
+import styled from 'styled-components'
+
+function TabContentOne() {
+    return (
+        <TabContentContainer>
+            <div className="container">
+                <div className="tab-content">
+                    <span>If you decide Netflix isn't for you - no problem. No Commitment.
+                        Cancel online anytime.</span>
+                    <br />
+                    <button>try it now</button>
+                    <img src={Img} />
+                </div>
+            </div>
+        </TabContentContainer>
+    )
+}
+
+export default TabContentOne;
+
+//Main Content Container
+const TabContentContainer = styled.div`
+    background: var(--main-deep-dark);
+
+    img {
+        width: 31.875rem;
+    }
+
+    .tab-content {
+        display:grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 2rem;
+        align-items: center;
+        font-size: 2rem;
+        padding: 2.5rem;
+    }
+`;
